7a25468effaef0d069473bd065bfeca8c78a1ed2
wbuck <wbuckley15@hotmail.com>
2023-11-12 13:53:55 -0500

    WIP - initial backup impl

--- azvm_manager/src/main.rs
+++ azvm_manager/src/main.rs
@@ -3,14 +3,23 @@ use azure_core::{RetryOptions, ExponentialRetryOptions, auth::TokenCredential, S
 use clap::{Parser, Subcommand, Args};
 use futures_util::{StreamExt, TryStreamExt};
 use std::sync::Arc;
-use std::task::Waker;
 use azure_core::headers::HeaderName;
 use log::debug;
 use store::Store;
 use azure_mgmt_resources::{Client as ResourceClient, models::ResourceGroup};
 use azure_mgmt_subscription::{Client as SubscriptionClient, models::Subscription};
 use azure_mgmt_recoveryservicesbackup::{Client as BackupClient};
-use azure_mgmt_recoveryservicesbackup::models::{WorkloadProtectableItem, WorkloadProtectableItemResource};
+use azure_mgmt_recoveryservicesbackup::models::{
+    AzureIaaSvmProtectedItem,
+    OperationStatus,
+    operation_status::Status as OpStatus,
+    ProtectedItem,
+    ProtectedItemResource,
+    ProtectedItemUnion,
+    Resource as RequestResource
+};
+use azure_mgmt_recoveryservicesbackup::models::operation_status::Status;
+use azure_mgmt_recoveryservicesbackup::models::protected_item::{BackupManagementType, WorkloadType};
 use tokio::time::{sleep_until, Duration, Instant};
 use dsp::{display_rg, display_sub, display_vm, Output};
 use spinoff::{Spinner, spinners, Color};
@@ -412,6 +421,8 @@ async fn process_vm_cmd(args: VmArgs, store: &Store, creds: Arc<dyn TokenCredent
 
             spinner.clear();
 
+            println!("{vm:#?}");
+
             display_vm(Output::Single(&vm));
         },
         VmCmd::List { group, sub_id } => {
@@ -504,14 +515,6 @@ async fn process_recovery_cmd(args: RecoveryArgs, store: &Store, creds: Arc<dyn 
             let vault_group = get_opt(&vault_group, || store.get_vault_resource_group().or_else(|| Some(group_name))
                 .ok_or(error::AppError::NoRg))?;
 
-            // let mut vm_names = match names {
-            //     Some(vm_names) => vm_names,
-            //     None => {
-            //         let client = VmClient::new(creds);
-            //         client.list_vm_names(group_name, subscription_id).await?
-            //     }
-            // };
-
             let mut spinner = Spinner::new(
                 spinners::Dots,
                 format!("Refreshing recovery services vault..."),
@@ -554,19 +557,193 @@ async fn process_recovery_cmd(args: RecoveryArgs, store: &Store, creds: Arc<dyn 
                 }
             }
 
-            spinner.stop_with_message("Refresh complete");
+            spinner.update_text("Getting list of virtual machines..");
 
-            let mut page = client.backup_protectable_items_client()
-                .list(vault_name, vault_group, subscription_id)
-                .filter("backupManagementType eq 'AzureIaasVM'")
-                .into_stream();
+            let vm_client = VmClient::new(creds);
 
-            while let Some(vms) = page.next().await {
-                let vms = vms?;
-                for vm in vms.value.iter() {
-                    println!("{vm:#?}");
+            let vm_names = names.unwrap_or_else(|| Vec::new());
+            let vms = vm_client.list_vms(group_name, subscription_id).await?;
+
+
+            let values = vms
+                .into_iter()
+                .filter_map(|vm| {
+                    if !vm_names.is_empty() && !vm_names.iter().any(|name| Some(name) == vm.resource.name.as_ref()) {
+                        return None;
+                    }
+                    match (vm.resource.name.as_ref(), vm.resource.id.as_ref()) {
+                        (Some(name), Some(id)) => {
+                            let container_name = format!("iaasvmcontainer;iaasvmcontainerv2;{group_name};{name}");
+                            let protected_item_name = format!("vm;iaasvmcontainerv2;{group_name};{name}");
+                            Some((container_name, protected_item_name, id.clone(), vm.resource))
+                        },
+                        _ => None
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            let mut items = Vec::new();
+            let total = values.len();
+            let mut count = 0;
+
+            spinner.update_text(format!("Protected {count}/{total} virtual machines"));
+
+            for (container_name, protected_item_name, id, resource) in values {
+                let resource = RequestResource {
+                    id: resource.id,
+                    name: resource.name,
+                    location: Some(resource.location),
+                    tags: resource.tags,
+                    e_tag: None,
+                    type_: resource.type_
+                };
+
+                let item = ProtectedItem {
+                    backup_management_type: Some(BackupManagementType::AzureIaasVm),
+                    workload_type: Some(WorkloadType::Vm),
+                    container_name: Some(container_name.clone()),
+                    source_resource_id: Some(id.clone()),
+                    policy_id: Some(format!("subscriptions/{subscription_id}/resourceGroups/{vault_name}/providers/microsoft.recoveryservices/vaults/{vault_name}/backupPolicies/DefaultPolicy")),
+                    last_recovery_point: None,
+                    backup_set_name: None,
+                    create_mode: None,
+                    deferred_delete_time_in_utc: None,
+                    is_scheduled_for_deferred_delete: None,
+                    deferred_delete_time_remaining: None,
+                    is_deferred_delete_schedule_upcoming: None,
+                    is_rehydrate: None,
+                    resource_guard_operation_requests: vec![],
+                    is_archive_enabled: None,
+                    policy_name: Some(String::from("DefaultPolicy")),
+                    soft_delete_retention_period_in_days: None,
+                };
+
+                let properties = AzureIaaSvmProtectedItem {
+                    protected_item: item,
+                    friendly_name: None,
+                    virtual_machine_id: None,
+                    protection_status: None,
+                    protection_state: None,
+                    health_status: None,
+                    health_details: vec![],
+                    kpis_healths: None,
+                    last_backup_status: None,
+                    last_backup_time: None,
+                    protected_item_data_id: None,
+                    extended_info: None,
+                    extended_properties: None,
+                };
+
+                let body = ProtectedItemResource {
+                    resource,
+                    properties: Some(ProtectedItemUnion::AzureIaaSvmProtectedItem(properties))
+                };
+                let response = client.protected_items_client().create_or_update(
+                    vault_name,
+                    vault_group,
+                    subscription_id, "Azure",
+                    container_name.as_str(),
+                    protected_item_name.as_str(),
+                    body
+                ).send().await?;
+
+
+                let location = response
+                    .as_ref()
+                    .headers()
+                    .get_optional_str(&HeaderName::from_static("location"))
+                    .ok_or(error::AppError::MissingLocationHeader)?;
+
+                let location = Url::parse(location)?;
+                let operation_id = location
+                    .path_segments()
+                    .expect("Invalid location header")
+                    .last()
+                    .unwrap();
+
+                let retry_secs: u64 = response
+                    .as_ref()
+                    .headers()
+                    .get_optional_str(&HeaderName::from_static("retry-after"))
+                    .unwrap()
+                    .parse()?;
+
+                sleep_until(Instant::now() + Duration::from_secs(retry_secs)).await;
+
+
+                loop {
+                    let status = client.protected_item_operation_statuses_client().get(
+                        vault_name,
+                        vault_group,
+                        subscription_id,
+                        "Azure",
+                        container_name.as_str(),
+                        protected_item_name.as_str(),
+                        operation_id
+                    ).await?;
+
+                    match status.status {
+                        Some(OpStatus::Succeeded) => {
+                            println!("Succeeded");
+
+                            let item = client.protected_item_operation_results_client().get(
+                                vault_name,
+                                vault_group,
+                                subscription_id,
+                                "Azure",
+                                container_name.as_str(),
+                                protected_item_name.as_str(),
+                                operation_id
+                            ).await?;
+
+                            items.push(item);
+
+                            count += 1;
+                            spinner.update_text(format!("Protected {count}/{total} virtual machines"));
+
+                            break;
+                        },
+                        Some(OpStatus::Failed) => {
+                            println!("Failed");
+                            break;
+                        },
+                        Some(OpStatus::InProgress) => {
+                            println!("In progress");
+                            continue;
+                        },
+                        Some(OpStatus::Invalid) => {
+                            println!("Invalid");
+                            break;
+                        },
+                        Some(OpStatus::Canceled) => {
+                            println!("Cancelled");
+                            break;
+                        },
+                        Some(OpStatus::UnknownValue(value)) => {
+                            println!("Unknown value: {value}");
+                            break;
+                        }
+                        None => continue
+                    }
                 }
+
             }
+
+            spinner.stop_with_message("All virtual machines protected");
+            println!("{items:#?}");
+
+
+            // let mut page = client.backup_protectable_items_client()
+            //     .list(vault_name, vault_group, subscription_id)
+            //     .filter("backupManagementType eq 'AzureIaasVM'")
+            //     .into_stream();
+            //
+            // while let Some(vms) = page.next().await {
+            //     let vms = vms?;
+            //     for vm in vms.value.iter() {
+            //         println!("{vm:#?}");
+            //     }
+            // }
         }
     }
 
